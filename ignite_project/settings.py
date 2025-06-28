6dde2a403331425e3028d0559badbbe3b5ff06a6
jsihin <jishin.cheruvattil@slots.com.kw>
2025-06-28 17:21:35 +0530

    environ

--- ignite_project/settings.py
+++ ignite_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!)vmw8x(48$jc0ze&^)dh8^l!0_tmq^h+r5du#aqk!@t29%h0'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%!)vmw8x(48$jc0ze&^)dh8^l!0_tmq^h+r5du#aqk!@t29%h0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
