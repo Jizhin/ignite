5db65ea4698011a36585a666021f91e875a4285a
jsihin <jishin.cheruvattil@slots.com.kw>
2025-06-30 16:46:46 +0530

    req

--- ignite_project/settings.py
+++ ignite_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ignite_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-
+print(DATABASE_URL)
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
